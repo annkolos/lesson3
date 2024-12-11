264bae0967d738d58b6ab14bf28609d30b56d300
annkolos <kotsad@mail.ru>
2024-12-11 12:58:23 +0300

    Add files via upload

---
+++ module_1_4.1.py
@@ -0,0 +1,8 @@
+klv_vpln_dz=12
+klv_ptrch_chsv=1.5
+nzv_krs='Python'
+vrm_n_zdn=klv_ptrch_chsv/klv_vpln_dz
+print('Курс:',nzv_krs,','
+      'всего задач:',klv_vpln_dz ,','
+      'затрачено часов:',klv_ptrch_chsv,','
+      'среднее время выполнения:',vrm_n_zdn,'часа')
